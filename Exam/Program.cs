503060c1bcb2a6221077e7ae141b7e10a38030f9
veryval <veryval@mail.ru>
2023-05-24 22:38:08 +0300

    Добавлен файл с программой

---
+++ Exam/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите исходный массив строк, разделенных пробелами:");
+        string[] inputArray = Console.ReadLine().Split();
+
+        string[] outputArray = FilterStrings(inputArray);
+
+        Console.WriteLine("Новый массив строк с длиной не более 3 символов:");
+        
+        foreach (string str in outputArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    static string[] FilterStrings(string[] inputArray)
+    {
+        int count = 0;
+
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] outputArray = new string[count];
+        int index = 0;
+
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                outputArray[index] = str;
+                index++;
+            }
+        }
+
+        return outputArray;
+    }
+}
